c7882baf0a2db71c982e3b1803bb0a6b975e5cdf
dit05 <dit@dit05ui-imac.local>
2019-04-17 15:30:48 +0900

    dd

--- datepicker/ViewController.swift
+++ datepicker/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var outLabel: UILabel!
+    @IBOutlet var myDatePicker: UIDatePicker!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let locale = Locale(identifier: "ko_KO")
+        myDatePicker.locale = locale
+        
+        // 모드 설정
+        myDatePicker.datePickerMode = UIDatePicker.Mode.dateAndTime
     }
-
+    @IBAction func valueC(_ sender: UIDatePicker) {
+        print(myDatePicker.date)
+        let d = myDatePicker.date
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm EEE a"
+        outLabel.text = formatter.string(from: d)
+    }
+    
 
 }
 
